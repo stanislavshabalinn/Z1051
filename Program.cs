1d4a4545d18d54b0858f9bc4cd7d3082adff6e33
stanislavshabalinn <shabalin_sa1@mail.ru>
2024-10-18 10:13:51 +0700

    исправления подписей

--- Program.cs
+++ Program.cs
@@ -10,48 +10,55 @@
             while (again == 'д')
             {
                 /// Объявляем переменные
-                double a; // В переменную a мы внесём первое число, которое запишет пользователь
-                double b; // В переменную b — второе число
-                double total; // Выводит результат математических операций с переменными а и b
-                char oper; //  В переменную мы будем заносить знак математической операции над числами
+                /// Переменную a заносит пользователь
+                /// Переменную b заносит пользователь
+                /// Пользователь заносит знак математической операции над числами
+                /// Результат математической операции
+                double a; 
+                double b; 
+                double total;
+                char oper; 
 
-                ///Cчитыват данные, которые пользователь будет вводить на консоль
-
-                Console.WriteLine("Введите первое число:"); // Вывод на консоль числа
-                a = Convert.ToDouble(Console.ReadLine());   // Введенные пользователем данные имеют строковой формат string, а так как у нашей переменной тип double, то конвертируем string в double методом Convert
-
-                Console.WriteLine("Введите оператор/действие:"); //Вывод на консоль знак математической операции
-                oper = Convert.ToChar(Console.ReadLine()); // конвертируем string в char,
-
-                Console.WriteLine("Введите второе число:"); //Вывод на консоль числа
-                b = Convert.ToDouble(Console.ReadLine());   // Введенные пользователем данные имеют строковой формат string, а так как у нашей переменной тип double, то конвертируем string в double методом Convert
-
+                ///Cчитывать данные, которые пользователь будет вводить на консоль
+                /// Вывод на консоль запрос первого числа а
+                Console.WriteLine("Введите первое число:");
+                /// Введенное число имеет строковой формат string, а так как у нашей переменной тип double, то конвертируем string в double методом Convert
+                a = Convert.ToDouble(Console.ReadLine());   
+                ///Вывод на консоль знак математической операции b
+                Console.WriteLine("Введите оператор/действие:"); 
+                /// конвертируем string в char
+                oper = Convert.ToChar(Console.ReadLine()); 
+                ///Вывод на консоль числа
+                Console.WriteLine("Введите второе число:");
+                /// Введенное число имеет строковой формат string, а так как у нашей переменной тип double, то конвертируем string в double методом Convert
+                b = Convert.ToDouble(Console.ReadLine());   
                 ///Выполнение одной операции над числами
-
                 if (oper == '+')
+                /// Вывод на консоль результата сложения
                 {
                     total = a + b;
-                    Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");  // Вывод на консоль результата сложения
+                    Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");  
                 }
-
+                /// Вывод на консоль результата вычетания
                 else if (oper == '-')
                 {
                     total = a - b;
-                    Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");   // Вывод на консоль результата вычетания
+                    Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");   
                 }
-
+                /// Вывод на консоль результата умножения
                 else if (oper == '*')
                 {
                     total = a * b;
-                    Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + "."); // Вывод на консоль результата умножения
+                    Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + "."); 
                 }
-
+                 /// Вывод на консоль результата деления
                 else if (oper == '/')
                 {
-                    try// добавил конструкцию Try/Catch/Finally для проверки корректности введённого значения
+                    /// Добавил конструкцию Try/Catch/Finally для проверки корректности введённого значения
+                    try
                     {
                         total = a / b;
-                        Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");   // Вывод на консоль результата деления
+                        Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");   
                     }
                     catch (FileNotFoundException ex)
                     {
@@ -61,16 +68,44 @@
                         Console.WriteLine("На ноль делить нельзя.");
                     }
                 }
-
+                 /// Вывод на консоль предупреждения, что неизвестный оператор
                 else
                 {
-                    Console.WriteLine("Неизвестный оператор."); // Вывод на консоль предупреждения, что неизвестный оператор
+                    Console.WriteLine("Неизвестный оператор."); 
                 }
-                Console.WriteLine("Вы хотите продолжить работу с калькулятором? (д/н)"); //  Продолжение/прекращение выполнения операций
+                ///  Вывод на консоль вопроса по продолжению или прекращению выполнения операций
+                Console.WriteLine("Вы хотите продолжить работу с калькулятором? (д/н)"); 
                 again = Convert.ToChar(Console.ReadLine());
             }
+        }
 
+ public class Logger
+        {
+            private const string FileName = "Logger.log";
 
+            public float Divide(float a, float b)
+            {
+                File.WriteAllText(FileName, $"Running {a} / {b}");
+                return a / b;
+            }
+
+            public float Multiply(float a, float b)
+            {
+                File.WriteAllText(FileName, $"Running {a} * {b}");
+                return a * b;
+            }
+
+            public float Add(float a, float b)
+            {
+                File.WriteAllText(FileName, $"Running {a} + {b}");
+                return a + b;
+            }
+
+            public float Subtract(float a, float b)
+            {
+                File.WriteAllText(FileName, $"Running {a} - {b}");
+                return a - b;
+            }
         }
     }
 }
